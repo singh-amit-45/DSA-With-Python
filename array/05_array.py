ab4c5a80e5dd5b9bf22c2fafe57f464b4b4077dc
Amit Singh <amitsingh100014@gmail.com>
2026-01-28 10:30:06 +0530

    use linspace to create an array of evenly spaced values

--- array/05_array.py
+++ array/05_array.py
@@ -1,3 +1,6 @@
+# find index of an element in an array with index() method
+
+
 from array import *
 array = array('i', [10, 20, 30, 40, 50, 60, 70, 80, 90])
 i = array.index(40)
---
+++ array/07_array.py
@@ -0,0 +1,5 @@
+from numpy import *
+
+val = linspace(0, 10, 5)
+for x in val:
+    print(x, end=" ")
