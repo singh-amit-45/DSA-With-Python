75830ea96191b751b73dc1f1e06868df04c9a23c
Amit Singh <amitsingh100014@gmail.com>
2026-01-28 10:52:01 +0530

    Add examples of ones() and full() functions in NumPy

---
+++ array/09_array.py
@@ -0,0 +1,11 @@
+# use ones() function to create an array of 10 ones and print each element
+from numpy import *
+
+val = ones(10)
+for x in val:
+    print(x, end=" ")
+
+
+# val = full(10, 5)  10 size ka array hoga  jisme har element 5 hoga
+# 5 5 5 5 5 5 5 5 5 5
+
